f78172cadacdfa08329d9d50a9ef083fd3ef3880
PranayPilaware <pranaypilaware22@gmail.com>
2025-08-17 23:56:21 +0530

    new code

--- instance.tf
+++ instance.tf
@@ -1,4 +1,12 @@
-resource "local_file" "test" {
-  content  = "Terraform is working!"
-  filename = "${path.module}/terraform_test.txt"
+provider "aws" {
+  region = "us-west-1"
+
+}
+resource "aws_instance" "myec2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = "new test.pm"
+  tags = {
+    Name ="spiderman instance"
+  
 }
