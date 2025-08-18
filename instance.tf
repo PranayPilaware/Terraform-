e3a4a7ef804f4b09878180e6066033fd0bdb683e
PranayPilaware <pranaypilaware22@gmail.com>
2025-08-18 13:50:17 +0530

    ami change

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami           = "ami-020cba7c55df1f615"
   instance_type = "t2.micro"
-  key_name      = "new test.pm"
+  key_name      = "id.pem"
   vpc_security_group_ids = ["mysg"]
   tags = {
     Name ="spiderman instance"
