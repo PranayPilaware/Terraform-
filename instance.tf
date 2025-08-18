dda459e115a8c572bccc4a597330bf0abaf75285
PranayPilaware <pranaypilaware22@gmail.com>
2025-08-18 13:35:11 +0530

    ami change

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
 
 }
 resource "aws_instance" "myec2" {
-  ami           = "ami-0de716d6197524dd9"
+  ami           = "ami-020cba7c55df1f615"
   instance_type = "t2.micro"
   key_name      = "new test.pm"
   vpc_security_group_ids = ["mysg"]
